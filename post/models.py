e3084a574ddd7ab4fe52686f06c1265c1f42bd03
Nelson Mendez <nelsonmendez.m08@gmail.com>
2024-10-05 17:40:45 -0400

    Modelo post

---
+++ post/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=40)
+    descripcion = models.TextField()
+    author = models.ForeignKey("auth.user", 
+        on_delete = models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = "publicaiones"
+
+    def __str__(self):
+        return self.titulo
+    
+class Personas(models.Model):
+
+    imagen=models.ImageField(upload_to="photos")
+    nombre=models.CharField(max_length=50)
+    descripcion=models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "personal"
+
+    def __str__(self):
+        return self.nombre
+    
